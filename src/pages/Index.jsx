a7024e1dc1071f69c77d7edcc666f4b17e531aa9
= <60821161+Agua-bit@users.noreply.github.com>
2021-11-05 13:23:56 -0500

    Adelanto viernes 5 noviembre 2021

---
+++ src/pages/Index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function index () {
+    return(
+        <div></div>
+    )
+}
+
+const Index = () => {
+    const []
+    return (
+        <div>
+            Hola, esta es la pagina de Indice
+            <input type="text" placeholder="Ingrese el nuevo valor"></input>
+        </div>
+    )
+}
+
+
+export default Index
